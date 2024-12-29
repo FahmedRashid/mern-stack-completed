5dd94f27ae58498a65c7015f24d9351124aab8ef
fahmed <fahmed7921@gmail.com>
2024-12-29 14:43:06 -0600

    took bcrypt package and userd salt and hash to encrypt the password upon signups and also added a if statement to check if the email is already in user. Also added a try catch to check if there are any errors for double checking the signup session.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,15 @@ const loginUser = async(req, res) =>{
 // signup user
 
 const signupUser = async(req, res) =>{
-    res.json({mssg: 'signup user'})
+
+    const {email, password} = req.body //grab the email and password from request body
+
+    try{
+        const user = await User.signup(email,password)
+        res.status(200).json({email, user})
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {signupUser, loginUser}
