69415e4c299a7b173a7a726d1bffe5c31d9cba46
fahmed <fahmed7921@gmail.com>
2025-01-05 18:10:49 -0600

    this will prevent the browser to hold the previous login state to show when a new user login.

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,12 +1,15 @@
 import { useAuthContext } from "./useAuthContext"
+import { useWorkoutsContext } from "./useWorkoutsContext"
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const {dispatch : workoutDispatch} = useWorkoutsContext() // this will prevent the browser to hold the previous login state to show when a new user login. 
     const logout = () =>{
         //remove user from storage.
         localStorage.removeItem('user')
         
         // dispatch logout action
         dispatch({type: 'LOGOUT'})
+        workoutDispatch({type: 'SET_WORKOUTS', payload: null}) // this will prevent the browser to hold the previous login state to show when a new user login. 
     }
     return {logout}
 }
